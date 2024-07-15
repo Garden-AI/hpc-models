110c89d71b641f191dcad54a8fdd912c2f00d60c
jisenli2 <jisenli2@illinois.edu>
2024-07-15 17:13:26 -0500

    Finished

--- new_job.sh
+++ new_job.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=16     # Match to OMP_NUM_THREADS
 #SBATCH --partition=gpuA40x4
-#SBATCH --time=00:10:00
+#SBATCH --time=00:40:00
 #SBATCH --account=bdao-delta-gpu   # Match to a "Project" returned by the "accounts" command
 #SBATCH --job-name=physnet_training
 ### GPU options ###
@@ -23,26 +23,35 @@ module load gcc/11.4.0
 module load openmpi/4.1.6
 module load cudnn/8.9.0.131
 
-# Get the current user's home directory
-HOME_DIR=$HOME
-
 # Define the environment path
-ENV_PATH="$HOME_DIR/.conda/envs/physnet_env"
+ENV_PATH="/projects/bdao/$USER/.conda/envs/physnet_env"
 
 # Check if the environment exists
 if [ ! -d "$ENV_PATH" ]; then
-    echo "Environment does not exist. Creating environment from env.yml..."
-    conda env create -f env.yml
+    echo "Environment does not exist. Creating environment from environment.yml..."
+    conda env create --prefix $ENV_PATH -f environment.yml
 else
-    echo "Environment exists. Updating environment from env.yml..."
-    conda env update --prefix $ENV_PATH --file env.yml --prune
+    echo "Environment exists. Updating environment from environment.yml..."
+    conda env update --prefix $ENV_PATH --file environment.yml --prune
+fi
+
+# Define the repository path
+REPO_PATH="/projects/bdao/$USER/physnet_hpc"
+
+# Check if the repository directory already exists
+if [ ! -d "$REPO_PATH" ]; then
+    echo "Repository directory does not exist. Cloning repository..."
+    # Clone the repository to the specified path
+    git clone https://github.com/Garden-AI/hpc-models.git $REPO_PATH
+else
+    echo "Repository directory exists. Pulling latest updates..."
+    # Navigate to the repository directory and pull latest updates
+    cd $REPO_PATH
+    git pull
 fi
 
 # Activate the environment
 source activate $ENV_PATH
-
-conda install pytorch torchvision torchaudio cudatoolkit=11.8 -c pytorch
-conda install pyg-lib -c pyg
 
 # Check if conda activation was successful
 if [ $? -ne 0 ]; then
