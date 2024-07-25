ce0227470425ae925cc76ecc11802ffc26bda3ec
Haoqi Chen <haoqic2@dt-login01.delta.ncsa.illinois.edu>
2024-07-25 12:25:28 -0500

    Apptainer physnet

---
+++ Apptainer/scripts/apptainer.py
@@ -0,0 +1,108 @@
+import logging
+
+import os
+import subprocess
+import tempfile
+import sys
+
+logger = logging.getLogger()
+
+
+
+
+# def start_apptainer(notebooks_dir, container_image):
+#     process = None
+#     try:
+#         with tempfile.TemporaryDirectory() as working_directory:
+#             tmp_dir = os.path.join(working_directory, "tmp")
+#             os.makedirs(tmp_dir, exist_ok=True)
+
+#             # Step 2: Set environment variables
+#             os.environ["SINGULARITY_TMPDIR"] = tmp_dir
+#             os.environ["APPTAINER_TMPDIR"] = tmp_dir
+#             os.environ["JUPYTER_RUNTIME_DIR"] = tmp_dir
+#             os.environ["JUPYTER_DATA_DIR"] = tmp_dir
+#             os.environ["JUPYTER_CONFIG_DIR"] = tmp_dir
+
+#             run_command = [
+#                 "apptainer",
+#                 "run",
+#                 "--bind",
+#                 f"{notebooks_dir}:/notebooks",
+#                 container_image,
+#                 "jupyter",
+#                 "notebook",
+#                 "--no-browser",
+#                 "--ip=0.0.0.0",
+#             ]
+#             process = subprocess.Popen(run_command)
+#             process.wait()
+#             logger.info(
+#                 "Jupyter Notebook started successfully in the Apptainer container."
+#             )
+#     except KeyboardInterrupt:
+#         if process:
+#             process.terminate()
+#             process.wait()
+#         logger.info("Operation cancelled by user (Ctrl-C).")
+       
+#         sys.exit(0)
+
+
+
+def rerun(container_image: str = "hpc-notebook.sif"):
+ 
+    container_image = os.path.abspath(os.path.join(os.path.dirname(__file__), "../hpc-notebook.sif"))
+    py_file = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../main.py"))
+
+    if not os.path.exists(container_image):
+        logger.error("Not found.")
+
+    else:
+        # start_apptainer(notebooks_dir, container_image)
+        command = ["apptainer", "exec", container_image, "bash", "-c", f"python3 {py_file}"]
+        subprocess.run(command, check=True)
+
+      
+
+# def start(container_image: str = "hpc-notebook.sif"):
+#     """Open a notebook file in HPC."""
+
+#     # Get the absolute path of the definition file located three levels up from the current script's directory
+#     definition_file = os.path.abspath(
+#         os.path.join(os.path.dirname(__file__), "Singularity.def")
+#     )
+    
+#     current_directory = os.getcwd()
+#     notebooks_dir = os.path.join(current_directory, "notebooks")
+
+#     os.makedirs(notebooks_dir, exist_ok=True)
+
+#     try:
+#         # Ensure the definition file exists
+#         if not os.path.isfile(definition_file):
+#             logger.error(f"Definition file {definition_file} not found.")
+  
+
+#         # Step 3: Build the Apptainer container
+#         build_command = ["apptainer", "build", container_image, definition_file]
+#         subprocess.run(build_command, check=True)
+#         logger.info("Apptainer container built successfully.")
+
+#         # Step 4: Run the Apptainer container and start Jupyter Notebook
+#         start_apptainer(notebooks_dir, container_image)
+#     except subprocess.CalledProcessError as e:
+#         logger.error(f"Failed to start Jupyter Notebook: {e}")
+
+#     except KeyboardInterrupt:
+#         logger.info("Operation cancelled by user (Ctrl-C).")
+        
+#         sys.exit(0)
+#     except Exception as e:
+#         logger.error(f"An error occurred: {e}")
+      
+
+
+# start()
+
+rerun()
---
+++ Apptainer/scripts/apptainer.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+#SBATCH --mem=64g
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=16     # Match to OMP_NUM_THREADS
+#SBATCH --partition=gpuA40x4
+#SBATCH --time=00:20:00
+#SBATCH --account=bdao-delta-gpu   # Match to a "Project" returned by the "accounts" command
+#SBATCH --job-name=physnet_training
+### GPU options ###
+#SBATCH --gpus-per-node=1
+#SBATCH --gpus-per-task=1
+#SBATCH --gpu-bind=verbose,per_task:1
+
+# Reset modules to a consistent state
+module reset
+
+# Load necessary modules
+# module load anaconda3_gpu
+module load cuda/11.8.0
+module load gcc/11.4.0
+module load openmpi/4.1.6
+
+
+
+
+
+
+echo "Job is starting on $(hostname)"
+echo "Current PATH: $PATH"
+
+# Set the number of threads to match CPUs per task
+export OMP_NUM_THREADS=16
+
+# Find the full path of the Python executable
+PYTHON_PATH=$(which python)
+echo "Python path: $PYTHON_PATH"
+
+# Check if the Python path is not empty
+if [ -z "$PYTHON_PATH" ]; then
+  echo "Python not found"
+  exit 1
+fi
+
+# Run the Python script using the full path and handle potential errors
+srun $PYTHON_PATH /u/haoqic2/project/hpc-models/Apptainer/scripts/apptainer.py > myjobs33.out
+if [ $? -ne 0 ]; then
+  echo "srun command failed"
+  exit 1
+fi
+
+
